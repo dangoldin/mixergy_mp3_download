301d16491cb361914d6d8b213b45a2f37cb8e75f
Dan Goldin <dangoldin@gmail.com>
2010-11-16 17:55:38 -0500

    Cleaned up code a bit

--- mixergy.py
+++ mixergy.py
@@ -1,43 +1,46 @@
-import os,sys,csv,urllib,time,re
+import os,sys,csv,urllib,time,re,itertools
 from BeautifulSoup import BeautifulSoup
 
-base_url = 'http://mixergy.com/interviews/page/2/'
+class MixergyMP3Retriever:
+    def __init__(self):
+        pass
 
-def getUrls(url):
-    print "Getting ",url
-    data = urllib.urlopen( url )
-    text = data.read()
-    text = text.replace("\r","")
-    text = text.replace("\n","")
-    
-    print text
-    soup = BeautifulSoup( text )
-    d = soup.findAll('h2', {'class':'title '})
-    print d
-    new_url = ''
-    for dd in d:
-        x = dd.find('a')
-        print x['href']
-        new_url = x['href']
-    findMP3(new_url)
+    def getListingUrls(self,start_pg, end_pg):
+        return ['http://mixergy.com/interviews/page/%d/' % i for i in range(start_pg,end_pg+1)]
+
+    def getIndividualUrls(self,listing_url):
+        print "Getting",listing_url
+        data = urllib.urlopen( listing_url )
+        time.sleep(3)
+        text = data.read().replace("\r","").replace("\n","")
+        soup = BeautifulSoup( text )
+        return [d.find('a')['href'] for d in soup.findAll('h2', {'class':'title '})]
         
-def findMP3(url):
-    print "Getting ",url
-    data = urllib.urlopen( url )
-    text = data.read()
-    text = text.replace("\r","")
-    text = text.replace("\n","")
-    soup = BeautifulSoup( text )
-    
-    link = soup.find('a', {'href': re.compile('mp3')})
-    print link['href']
+    def findMP3Url(self,url):
+        print "Getting",url
+        data = urllib.urlopen( url )
+        time.sleep(3)
+        text = data.read().replace("\r","").replace("\n","")
+        soup = BeautifulSoup( text )
+        #link = soup.find('a', {'href': re.compile('mp3')})
+        return soup.find('a', {'href': re.compile('mp3')})['href']
+        #print link['href']
     
 def main(argv=None):
+#    m = MixergyMP3Retriever()
+#    listing_urls = m.getListingUrls(1, 1)
+    #individual_urls = [m.getIndividualUrls(listing_url) for listing_url in listing_urls]
+#    individual_urls = list(itertools.chain(*[m.getIndividualUrls(listing_url) for listing_url in listing_urls]))
+#    print len(individual_urls),"-",individual_urls
+#    mp3_urls = [m.findMP3Url(url) for url in individual_urls]
+#    print len(mp3_urls),"-",mp3_urls
+    urllib.urlretrieve('http://mixergy.com/wp-content/audio/Rob-Rawson-(TimeDoctor)-on-Mixergy.mp3', '/dev/mixergy/temp.mp3')
+    
     #if argv is None:
     #    argv = sys.argv
     #process_file( argv[1], argv[2] )
     #process_file('/dev/geo_data/geo_data.csv', '/dev/geo_data/geo_data_out.csv' )
-    getUrls(base_url)
+    #getUrls(base_url)
 
 if __name__ == '__main__':
     #main(sys.argv)
